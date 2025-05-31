430cb8d2f8344aa966473de9fe0261c1f69009ea
Marcel Elias <marcel@dromzeh.dev>
2025-06-01 00:53:28 +0100

    feat(stats): countries served, justify between

--- src/app/components/overall-stats.tsx
+++ src/app/components/overall-stats.tsx
@@ -6,22 +6,31 @@ export function OverallStats() {
         <div className="w-full py-8">
             <div className="max-w-4xl mx-auto">
                 <SectionHeader>Metrics & Impressions</SectionHeader>
-                <div className="grid md:grid-cols-2 grid-cols-1 gap-8 md:gap-16">
-                    <div className="flex flex-col gap-2">
-                        <p className="text-2xl md:text-3xl text-white font-instrument-serif">
-                            5B+
-                        </p>
-                        <p className="text-zinc-400 text-sm">
-                            Social media impressions
-                        </p>
-                    </div>
-
+                <div className="flex flex-col md:flex-row justify-between w-full gap-8">
                     <div className="flex flex-col gap-2">
                         <p className="text-2xl md:text-3xl text-white font-instrument-serif">
                             300K+
                         </p>
                         <p className="text-zinc-400 text-sm">
-                            Monthly active users
+                            Monthly Active Users
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <p className="text-2xl md:text-3xl text-white font-instrument-serif">
+                            5B+
+                        </p>
+                        <p className="text-zinc-400 text-sm">
+                            Social Media Impressions
+                        </p>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <p className="text-2xl md:text-3xl text-white font-instrument-serif">
+                            150+
+                        </p>
+                        <p className="text-zinc-400 text-sm">
+                            Countries Served
                         </p>
                     </div>
                 </div>
